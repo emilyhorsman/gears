a1382a76c031ba7274b817ac31ad06bd14c590c5
Emily Horsman <me@emilyhorsman.com>
2022-04-21 06:09:40 -0700

    user assignable teeth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { getBestGearPath } from "./Gearing";
 import Chart from "./Chart";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function calcs(gear, redundant) {
   const { front, rear, ratio } = gear;
@@ -9,14 +9,16 @@ function calcs(gear, redundant) {
   return { ...gear, speedAt1RPM, label: `${front}/${rear}t`, redundant };
 }
 
-const chainrings = [30, 46]; // [22, 32, 44].reverse();
-const cassette = [11, 13, 15, 17, 19, 22, 25, 28, 32, 36].reverse(); //[11, 13, 15, 17, 20, 23, 26, 32].reverse();
-const bestPath = getBestGearPath(chainrings, cassette);
-const gears = bestPath.map((g) => calcs(g, false));
-
 function App() {
   const [minRPM, setMinRPM] = useState(85);
   const [maxRPM, setMaxRPM] = useState(100);
+  const [chainrings, setChainrings] = useState([30, 46]);
+  const [cassette, setCassette] = useState([
+    11, 13, 15, 17, 19, 22, 25, 28, 32, 36,
+  ]);
+  const bestPath = getBestGearPath(chainrings, cassette);
+  const gears = bestPath.map((g) => calcs(g, false));
+
   return (
     <>
       <div>
@@ -42,6 +44,10 @@ function App() {
           step={1}
         />
       </div>
+      <div>
+        <ArrayInput value={chainrings} onChange={setChainrings} />
+        <ArrayInput value={cassette} onChange={setCassette} />
+      </div>
       <Chart
         gears={gears}
         bailOutRPM={50}
@@ -53,6 +59,67 @@ function App() {
       />
     </>
   );
+}
+
+function ArrayInput({ value, onChange }) {
+  const [text, setText] = useState(value.join(", "));
+  const prevValue = useRef();
+  useEffect(() => {
+    prevValue.current = value;
+  });
+  useEffect(() => {
+    // Make this component fully controlled but only update the text if the semantic
+    // value received from above is different.
+    if (!arrayEq(prevValue, value)) {
+      setText(value.join(", "));
+      prevValue.current = value;
+    }
+  }, [value]);
+
+  return (
+    <input
+      type="text"
+      value={text}
+      onChange={(event) => {
+        const newText = event.target.value;
+        const candidate = convert(newText);
+        setText(newText);
+        if (candidate != null) {
+          onChange(candidate);
+        }
+      }}
+    />
+  );
+}
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+function arrayEq(a, b) {
+  if (a.length != b.length) {
+    return false;
+  }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function convert(text) {
+  const arr = text.split(",").map((x) => Number(x));
+  for (let i = 0; i < arr.length; i++) {
+    if (Number.isNaN(arr[i]) || (i > 0 && arr[i] <= arr[i - 1])) {
+      return null;
+    }
+  }
+  return arr;
 }
 
 export default App;
