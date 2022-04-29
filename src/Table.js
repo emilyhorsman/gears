34ae2717dc7b8ec067389329a18637402b436f48
Emily Horsman <me@emilyhorsman.com>
2022-04-29 16:49:42 -0700

    basic table styling

--- src/Table.js
+++ src/Table.js
@@ -1,19 +1,9 @@
-import "./Table.css";
 import { RatioFormatter, GearInchesFormatter } from "./Utils";
 import { Fragment } from "react";
 
 function Table({ drivetrains }) {
-  const maxRearGears = Math.max(
-    ...drivetrains.map(({ params: { rears } }) => rears.length)
-  );
-
   return (
-    <div
-      className="table"
-      style={{
-        gridTemplateColumns: `repeat(${maxRearGears + 1}, max-content)`,
-      }}
-    >
+    <div>
       {drivetrains.map((drivetrain) => (
         <Drivetrain drivetrain={drivetrain} key={drivetrain.params.id} />
       ))}
@@ -23,36 +13,36 @@ function Table({ drivetrains }) {
 
 function Drivetrain({ drivetrain }) {
   return (
-    <>
-      <div className="title">{drivetrain.title}</div>
-      {drivetrain.params.rears
-        .slice()
-        .reverse()
-        .map((rear, index) => (
-          <div key={rear} style={{ gridColumn: index + 2 }} className="rear">
-            {rear}t
-          </div>
-        ))}
-      {drivetrain.byChainring.map((gears) => {
-        const frontTeeth = gears[0].params.front;
-        return (
-          <Fragment key={`${drivetrain.id}-${frontTeeth}`}>
-            <div style={{ gridColumn: 1 }} className="front">
-              {frontTeeth}t
-            </div>
-            {gears.map((gear) => (
-              <div key={gear.gainRatio}>
-                {RatioFormatter.format(gear.gainRatio)}
-                <br />
-                {GearInchesFormatter.format(gear.gearInches)}"
-                <br />
-                {GearInchesFormatter.format(gear.development.m)} m
-              </div>
+    <table>
+      <thead>
+        <tr>
+          <th />
+          {drivetrain.params.rears
+            .slice()
+            .reverse()
+            .map((rear) => (
+              <th key={rear}>{rear}t</th>
             ))}
-          </Fragment>
-        );
-      })}
-    </>
+        </tr>
+      </thead>
+      <tbody>
+        {drivetrain.byChainring.map((gears) => {
+          const frontTeeth = gears[0].params.front;
+          return (
+            <tr key={`${drivetrain.id}-${frontTeeth}`}>
+              <th style={{ gridColumn: 1 }} className="front">
+                {frontTeeth}t
+              </th>
+              {gears.map((gear) => (
+                <td key={gear.gainRatio}>
+                  {RatioFormatter.format(gear.gainRatio)}
+                </td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
 
